ba728f38b8dd1c839a092e15fdfc8713e2723cb0
pascalria <116817813+pascalria@users.noreply.github.com>
2022-11-10 00:42:44 +0800

    Update storeHash.sol

--- contracts/storeHash.sol
+++ contracts/storeHash.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.0;
 
+import "./Checksign.sol";
+
+contract chk is Checksign{
+
+}
+
 contract storeHash{
 
     mapping(address => mapping(uint => FileHash)) stores;
@@ -7,24 +13,26 @@ contract storeHash{
  
     struct FileHash{
         uint id ;
-        bytes32 hash ;
+        bytes signedhash ;
     }
 
     FileHash filehash;
 
-    function hashIn(uint id,bytes32 hash) public {
+    function hashIn(uint id,bytes signedhash) public {
         require(hashInStore[id]==address(0));
         FileHash memory h1 = filehash;
         h1.id = id;
-        h1.hash = hash;
+        h1.signedhash = signedhash;
         stores[msg.sender][id] = h1;
         hashInStore[id] = msg.sender;
     }
 
-    function checkHash(uint id1,bytes32 hash1) public view returns(bool,bytes32,bytes32){
-        FileHash memory h1 = stores[hashInStore[id1]][id1];
-        return(h1.hash == hash1,h1.hash,hash1);
+    function checkHash(chk chk1,uint id1,bytes32 hash,address pk) public view returns(bool){
+        FileHash h1 = stores[hashInStore[id1]][id1];
+        bytes signedhash1 = h1.signedhash;
+        bool result = chk1.check(signedhash1,hash,pk);
+        return result;
     }
 
-
 }
+
