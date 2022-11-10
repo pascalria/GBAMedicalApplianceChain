e48164555759bdaed4263471dcc3d2b32fc8ead4
pascalria <116817813+pascalria@users.noreply.github.com>
2022-11-10 12:05:43 +0800

    Update scan_notice.py

--- database/scan_notice.py
+++ database/scan_notice.py
@@ -1,16 +1,27 @@
 import pymysql
 
-db = pymysql.connect(
-    host="127.0.0.1"
-    port=3306
-    user="hospitaldb"
-    password="123456"
-    charset="utf8mb4"
-)
+def scan(db1, medid):
+    cursor = db1.cursor()
+    scan = "SELECT ID FROM hospitaltable WHERE medical_id==%s " %medid
+    cursor.execute(scan)
+    result = cursor.fetchall()
+    return result
 
-cursor = db.cursor()
+def create(db1):
+    cursor = db1.cursor()
+    create = ""
 
-scan = ""
 
-cursor.execute(scan)
-result = cursor.fetchall()
+if __name__ == "__main__":
+
+    db = pymysql.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="hospitaldb",
+        password="123456",
+        charset="utf8mb4"
+    )
+
+    medical_id = input("please input id of medical appliance")
+
+
