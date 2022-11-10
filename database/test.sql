213e54e611a89618bcfcfb8d4eecb6abd83a8e49
pascalria <116817813+pascalria@users.noreply.github.com>
2022-11-10 18:23:09 +0800

    Update test.sql

--- database/test.sql
+++ database/test.sql
@@ -1,1 +1,10 @@
-set
+CREATE TABLE 'hospital'(
+patient_id char(8) NOT NULL PRIMARY KEY COMMENT '患者id，为8位数字',
+hospital_addr BINARY(160) NOT NULL COMMENT '医院账户地址，以二进制存储'
+)
+
+CREATE TABLE 'medical'(
+FOREIGN KEY(patient_id) REFERENCES hospital(patient_id),
+medid char(4) NOT NULL COMMENT '药械id',
+remain_time tinyint NOT NULL COMMENT '如不为0，则代表离特定时间点得时间'
+)
